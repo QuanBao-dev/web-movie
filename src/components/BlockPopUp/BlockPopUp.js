b3fb28b858e6d376425b7aa038c97b28e30418fe
Quan Bao <quy123bao20@gmail.com>
2020-07-22 14:25:52 +0700

    get loading element to the center

--- src/components/BlockPopUp/BlockPopUp.js
+++ src/components/BlockPopUp/BlockPopUp.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./BlockPopUp.css";
 
-const BlockPopUp = ({todoState}) => {
+const BlockPopUp = ({ todoState }) => {
+  console.log(todoState);
   return (
     <div
       className="block-popup"
@@ -14,7 +15,9 @@ const BlockPopUp = ({todoState}) => {
             : "none",
       }}
     >
-      <h1>Loading...</h1>
+      <div>
+        <h1>Loading...</h1>
+      </div>
     </div>
   );
 };

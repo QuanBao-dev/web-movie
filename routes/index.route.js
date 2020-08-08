fcf1ef3eea12aa623ccf6997b823bc558fdf3bd9
Quan Bao <quy123bao20@gmail.com>
2020-08-09 00:11:21 +0700

    done render route

---
+++ routes/index.route.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const path = require("path");
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/anime/:malId/watch/:episode", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/anime/:name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/edit", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/auth/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+router.get("/auth/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+module.exports = router;

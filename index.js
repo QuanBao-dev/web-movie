2499de1c4f332ce07649e53d71da8ab3da819c95
Quan Bao <quy123bao20@gmail.com>
2020-08-09 00:09:07 +0700

    set up route

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ require("dotenv").config();
 const express = require('express')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const app = express()
 const port = 5000;
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
-  useFindAndModify:false,
+  useFindAndModify:false
+},(err) => {
+  if(err){
+    console.error(err)
+  } else {
+    console.log("connected to db")
+  }
 })
-// const moviesRoute = require("./routes/movies.route");
+const moviesRoute = require("./routes/movies.route");
 const usersRoute = require("./routes/users.route");
 const tokenRoute = require("./routes/token.route");
+const boxMovieRoute = require("./routes/boxMovie.route");
+const renderRoute = require("./routes/index.route");
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -22,8 +33,9 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
-
-// app.use('/api/movies', moviesRoute);
+app.use("/api/movies/box",boxMovieRoute);
+app.use('/api/movies', moviesRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/', tokenRoute);
+app.use("/",renderRoute);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

a42e9c895212ca73c6bafcbbbcc6189ffc94fff2
Quan Bao <quy123bao20@gmail.com>
2020-08-09 00:11:00 +0700

    done route users

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const {
   loginValidation,
   registerValidation,
+  changeInfoAccountValidation,
 } = require("../validations/user.validation");
 const User = require("../models/user.model");
 const { compare } = require("bcryptjs");
@@ -10,26 +11,18 @@ const ignoreProps = require("../validations/ignore.validation");
 const jwt = require("jsonwebtoken");
 const { verifyRole } = require("../middleware/verify-role");
 const { verifyLogin } = require("../middleware/verify-login");
+const BoxMovie = require("../models/boxMovie.model");
+const {
+  verifyChangeInfoUser,
+} = require("../middleware/verify-change-info-user");
 
 router.get("/", verifyRole("Admin"), async (req, res) => {
-  //TODO Get all user if you are admin
   try {
     const users = await User.find({});
     res.send({
-      message: users.map((user) => user.toJSON()),
-    });
-  } catch (error) {
-    res.status(404).send({ error: "Something went wrong" });
-  }
-});
-
-router.get("/id", verifyRole("Admin", "User"), async (req, res) => {
-  //TODO Get all user if you are admin
-  const { email } = req.user;
-  try {
-    const user = await User.findOne({ email });
-    res.send({
-      message: user.id
+      message: users.map((user) =>
+        ignoreProps(["password", "__v"], user.toJSON())
+      ),
     });
   } catch (error) {
     res.status(404).send({ error: "Something went wrong" });
@@ -55,11 +48,19 @@ router.post("/login", verifyLogin, async (req, res) => {
     const token = jwt.sign(userVm, process.env.JWT_KEY, {
       expiresIn: "12h",
     });
-    res.cookie("idCartoonUser", token, {
+    let options = {
       expires: new Date(Date.now() + 43200000),
       path: "/",
       signed: true,
-    });
+      secure: false,
+    };
+    if (process.env.NODE_ENV === "production") {
+      options = {
+        ...options,
+        httpOnly: true,
+      };
+    }
+    res.cookie("idCartoonUser", token, options);
     res.send({ message: token });
   } catch {
     res.status(404).send({ error: "Something went wrong" });
@@ -90,45 +91,104 @@ router.post("/register", verifyLogin, async (req, res) => {
 });
 
 router.delete("/logout", verifyRole("Admin", "User"), (req, res) => {
-  res.cookie("idCartoonUser", "", {
-    expires: new Date(Date.now() - 43200000),
+  let options = {
+    expires: new Date(Date.now() + 43200000),
     path: "/",
     signed: true,
-  });
+  };
+  if (process.env.NODE_ENV === "production") {
+    options = {
+      ...options,
+      httpOnly: true,
+    };
+  }
+
+  res.cookie("idCartoonUser", "", options);
   res.send({ message: "Log out success" });
 });
 
-router.put("/:id", verifyRole("Admin", "User"), async (req, res) => {
-  //TODO modify only your user info
-  const { id } = req.params;
-  const user = await User.findById(id);
-  const userSignIn = req.user;
-  if (user.email !== userSignIn.email) {
-    return res
-      .status(400)
-      .send({ error: "Can't change information of other user" });
+router.put(
+  "/current",
+  verifyRole("Admin", "User"),
+  verifyChangeInfoUser,
+  async (req, res) => {
+    let data = {};
+    if (req.body.newEmail) {
+      data = {
+        ...data,
+        email: req.body.newEmail,
+      };
+    }
+    if (req.body.newUsername) {
+      data = {
+        ...data,
+        username: req.body.newUsername,
+      };
+    }
+    if (req.body.newPassword) {
+      data = {
+        ...data,
+        password: req.body.newPassword,
+      };
+    }
+
+    const result = changeInfoAccountValidation(data);
+    if (result.error) {
+      return res.status(400).send({ error: result.error.details[0].message });
+    }
+    const user = req.validatedUser;
+    if (req.body.newUsername) {
+      user.username = req.body.newUsername;
+    }
+    if (req.body.newEmail) {
+      user.email = req.body.newEmail;
+    }
+    const emailExist = await User.findOne({ email: req.body.newEmail });
+    if (emailExist) {
+      return res.status(400).send({ error: "Email already existed" });
+    }
+    if (req.body.newPassword) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.newPassword, salt);
+    }
+    ///change cookie
+    try {
+      const userSaved = await user.save();
+      const userVm = ignoreProps(
+        ["_id", "__v", "password"],
+        userSaved.toJSON()
+      );
+      const token = jwt.sign(userVm, process.env.JWT_KEY);
+      let options = {
+        expires: new Date(Date.now() + 43200000),
+        path: "/",
+        signed: true,
+        secure: false,
+      };
+      if (process.env.NODE_ENV === "production") {
+        options = {
+          ...options,
+          httpOnly: true,
+        };
+      }
+      res.cookie("idCartoonUser", token, options);
+      res.send({
+        message: token,
+      });
+    } catch {
+      res.status(404).send({ error: "Something went wrong" });
+    }
   }
-  if (req.body.password) {
-    const salt = await bcrypt.genSalt(10);
-    req.body.password = await bcrypt.hash(req.body.password, salt);
-  }
-  try {
-    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-    res.send({
-      message: ignoreProps(["_id", "__v", "password"], user.toJSON()),
-    });
-  } catch {
-    res.status(404).send({ error: "Something went wrong" });
-  }
-});
+);
 
 router.delete("/:id", verifyRole("Admin"), async (req, res) => {
-  //TODO delete any user you want when you got permission Admin role
   const { id } = req.params;
   try {
     const user = await User.findByIdAndDelete(id, req.body);
+    const deleteBoxMovie = await BoxMovie.deleteMany({ user: id });
+    console.log(deleteBoxMovie.n);
     res.send({
-      message: ignoreProps(["id", "__v", "password"], user.toJSON()),
+      message: ignoreProps(["_id", "__v", "password"], user.toJSON()),
     });
   } catch {
     res.status(404).send({ error: "Something went wrong" });

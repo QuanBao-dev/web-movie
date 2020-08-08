ae8a5300512f509735d5ab8a02a89bf7110dbb05
Quan Bao <quy123bao20@gmail.com>
2020-08-09 00:12:03 +0700

    creating nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import './App.css';
+import "./App.css";
 
-import Axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react';
-import { useCookies } from 'react-cookie';
-import { BrowserRouter as Router, NavLink as Link, Route, Switch } from 'react-router-dom';
+import Axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { useCookies } from "react-cookie";
+import {
+  BrowserRouter as Router,
+  NavLink as Link,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 // import Chat from './Chat/Chat';
-import EpisodePage from './EpisodePage/EpisodePage';
-import Home from './Home/Home';
-import Login from './Login/Login';
-import Name from './Name/Name';
-import Register from './Register/Register';
-import { fetchingUser$, userStream } from './epics/user';
+import EpisodePage from "./EpisodePage/EpisodePage";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Name from "./Name/Name";
+import Register from "./Register/Register";
+import { fetchingUser$, userStream } from "./epics/user";
+import AdminManager from "./AdminManager/AdminManager";
+import { allowShouldFetchAllUser } from "./store/admin";
+import EditUser from "./EditUser/EditUser";
 
 function App() {
   const navLoginRef = useRef();
   const navRegisterRef = useRef();
   const [user, setUser] = useState(userStream.initialState);
   // eslint-disable-next-line no-unused-vars
-  const [cookies, setCookie, removeCookie] = useCookies(["idCartoonUser"]);
+  const [cookies, setCookie] = useCookies(["idCartoonUser"]);
 
   useEffect(() => {
     const subscription = userStream.subscribe(setUser);
@@ -34,10 +42,34 @@ function App() {
       <nav>
         <ul className="nav-bar__app">
           <li>
-            <Link to="/" activeClassName="active" exact>
+            <Link
+              to="/"
+              activeClassName="active"
+              onClick={() =>
+                // eslint-disable-next-line no-restricted-globals
+                scroll({
+                  top: 0,
+                  behavior: "smooth",
+                })
+              }
+              exact
+            >
               Home
             </Link>
           </li>
+          {user && user.role === "Admin" && (
+            <li>
+              <Link
+                to="/admin"
+                onClick={() => {
+                  allowShouldFetchAllUser(true);
+                }}
+                activeClassName="active"
+              >
+                Admin
+              </Link>
+            </li>
+          )}
           <li ref={navLoginRef} style={{ margin: "0 0 0 auto" }}>
             {!user && (
               <Link to="/auth/login" activeClassName="active">
@@ -64,17 +96,22 @@ function App() {
             </li>
           )}
           {user && (
-            <li style={{ color: "white" }}>
-              <div>{user.username}</div>
+            <li style={{ color: "white", cursor:"pointer" }}>
+              <Link to={`/edit`}>
+                {user.username}
+              </Link>
             </li>
           )}
         </ul>
       </nav>
       <Switch>
         <Route path="/" component={Home} exact />
-        {/* <Route path="/users/discuss" component={Chat} /> */}
-        <Route path="/anime/:malId/comment" component={EpisodePage} />
+        {user && user.role === "Admin" && (
+          <Route path="/admin" component={AdminManager} exact />
+        )}
+        <Route path="/anime/:malId/watch/:episode" component={EpisodePage} />
         <Route path="/anime/:name" component={Name} />
+        {user && <Route path="/edit" component={EditUser} />}
         {!user && <Route path="/auth/login" component={Login} />}
         {!user && <Route path="/auth/register" component={Register} />}
       </Switch>
@@ -96,7 +133,6 @@ async function logoutUser(setCookie, cookie) {
     userStream.updateUser(undefined);
     // window.location.replace("/");
   } catch (error) {
-    console.error(error);
   }
 }
 export default App;

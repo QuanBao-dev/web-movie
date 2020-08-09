a010229810401da3aebe5d42e4f1df6e8d1ae292
Quan Bao <quy123bao20@gmail.com>
2020-08-09 08:58:17 +0700

    set env PORT

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI,{
   useNewUrlParser:true,
   useUnifiedTopology:true,

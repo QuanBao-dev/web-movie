58d4ac10f6af05a296a3b4444a5a7735cfb6a0a7
Quan Bao <quy123bao20@gmail.com>
2020-08-09 10:13:13 +0700

    fix latest update

--- models/updatedMovie.js
+++ models/updatedMovie.js
@@ -13,4 +13,10 @@ updatedMovieSchema.pre("save",function(next){
   this.updatedAt= new Date(Date.now());
   next()
 })
+
+updatedMovieSchema.pre("findOneAndUpdate",function(next){
+  this._update.updatedAt= new Date(Date.now());
+  next()
+})
+
 module.exports = mongoose.model("updatedMovie", updatedMovieSchema);
